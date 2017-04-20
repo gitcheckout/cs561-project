6afbeb242f996ed3805ca004938b94bc272a5000
Divyanshu <k.divyanshu@iitg.ernet.in>
2017-04-20 23:05:38 +0530

    Minor tweaks

--- features_io.py
+++ features_io.py
@@ -18,32 +18,16 @@ def generate_feature_mat(folder="a", train=True):
         wav_file = "training/training-{}/{}.wav".format(
             folder, training_df.iloc[i]["filename"])
         mfcc_feat = mfcc_features(wav_file)
-        # print("mfcc_feat done for {}".format(i))
-        # normalization
-        # do not do normalization. It reduces accuracy
-        # mfcc_feat /= np.max(np.abs(mfcc_feat), axis=0)
         logfbank_feat = logfbank_features(wav_file)
-        # print("logfbank_feat done for {}".format(i))
-        # normalization
-        # do not do normalization. It reduces accuracy
-        # logfbank_feat /= np.max(np.abs(logfbank_feat), axis=0)
         ssc_feat = ssc_features(wav_file)
-        zcr_feat = zcr_features(wav_file)
-        # print("ssc_feat done for {}".format(i))
 
         comb_feat = np.append(mfcc_feat, logfbank_feat)
         comb_feat = np.append(comb_feat, ssc_feat)
-        # comb_feat = np.append(comb_feat, zcr_feat)
         features_df = features_df.append([comb_feat], ignore_index=True)
-    
-    # pprint.pprint(features_df.shape)
-    # pprint.pprint(features_df.head())
 
     # labels
     labels = pd.DataFrame()
     for i in range(0, len(training_df)):
         labels = labels.append([training_df.iloc[i]["abnormal"]], ignore_index=True)
-    # pprint.pprint(labels)
 
     return features_df, labels
-
--- train.py
+++ train.py
@@ -1,25 +1,60 @@
+import pprint
 import numpy as np
+import pandas as pd
 from sklearn import svm
 from sklearn.metrics import accuracy_score, f1_score, matthews_corrcoef
 
 from features_io import generate_feature_mat
 
 
-def train_and_classify(folder='a'):
-    # training data
-    training_data, training_labels = generate_feature_mat(folder)
-    #assert len(training_data) == len(training_labels)
+def train_and_classify(folder='a', all_folders=False, folders=None):
 
-    testing_data, testing_labels = generate_feature_mat(folder, train=False)
-    #assert len(testing_data) == len(testing_labels)
+    if not folders:
+        folders = ['a', 'b', 'c', 'd', 'e', 'f']
 
-    print("Features extracted")
+    if not all_folders:
+        training_data, training_labels = generate_feature_mat(folder)
+    else:
+        training_df_names = []
+        training_label_names = []
+        for folder in folders:
+            folder_td, folder_tl = generate_feature_mat(folder)
+            print("Type of tl is {}".format(type(folder_tl)))
+            training_df_names.append(folder_td)
+            training_label_names.append(folder_tl)
+        
+        training_labels = pd.concat(training_label_names)
+        training_data = pd.concat(training_df_names)
+        print("Shape of training data is: {}".format(training_data.shape))
+        print("Shape of training labels is: {}".format(training_labels.shape))
+        print("Training Labels are: ")
+
+
+    if not all_folders:
+        testing_data, testing_labels = generate_feature_mat(folder, train=False)
+    else:
+        testing_df_names = []
+        testing_label_names = []
+        for folder in folders:
+            folder_td, folder_tl = generate_feature_mat(folder)
+            testing_df_names.append(folder_td)
+            testing_label_names.append(folder_tl)
+
+        testing_labels = pd.concat(testing_label_names)
+        testing_data = pd.concat(testing_df_names)
+        print("Shape of testing data is: {}".format(testing_data.shape))
+
+    print("Classifier running:")
+    # SVM classifier
     clf = svm.SVC()
+    print(training_labels.shape)
     clf.fit(training_data, training_labels.values.ravel())
-    print("Training completed")
+    
+    print("Predicting")
     predictions = clf.predict(testing_data)
-
+    
+    # scores
     score1 = accuracy_score(testing_labels, predictions)
-    score2 = f1_score(testing_labels, predictions)
-    score3 = matthews_corrcoef(testing_labels, predictions)
+    score2 = f1_score(np.atleast_1d(testing_labels), predictions)
+    score3 = matthews_corrcoef(np.atleast_1d(testing_labels), predictions)
     print(score1, score2, score3)
